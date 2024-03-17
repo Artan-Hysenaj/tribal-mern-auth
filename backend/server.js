af6d5d3085579542a3ce650da8220116cfe49d10
Artan Hysenaj <ahysenaj@i-aps.com>
2024-03-17 11:48:35 +0100

    Add production configuration for serving static files and handling routes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -17,7 +18,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
-app.get("/", (req, res) => res.send("Server ready"));
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.get("*", (_, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => res.send("Server ready"));
+}
+
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => console.log("Server listening", port));
